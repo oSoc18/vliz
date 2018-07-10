60cd4dd91dc3a1a050fc7ce927665eac87641563
Pietervdvn <pietervdvn@gmail.com>
2018-07-10 14:33:40 +0200

    Added toString methods

--- OpenSeaLab/src/seabedhabitat/feature/Polygon.java
+++ OpenSeaLab/src/seabedhabitat/feature/Polygon.java
@@ -4,8 +4,19 @@ import java.util.List;
 
 public class Polygon {
 	
+	private final List<Point> points;
+	
 	public Polygon(List<Point> points) {
-		
+		this.points = points;
+	}
+	
+	@Override
+	public String toString() {
+		String result = "";
+		for (Point point : points) {
+			result += point.toString()+9", ";
+		}
+		return result;
 	}
 
 }
