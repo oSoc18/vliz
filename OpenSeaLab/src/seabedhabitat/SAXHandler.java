b7e7b0bace9154d33a78c39e043d9ea13bb44bbb
Isma <isma@localhost.localdomain>
2018-07-10 11:27:01 +0200

    Adding Feature and SAXHandler class

---
+++ OpenSeaLab/src/seabedhabitat/SAXHandler.java
@@ -0,0 +1,27 @@
+package seabedhabitat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class SAXHandler extends DefaultHandler {
+	private List<Feature> list = new ArrayList<>();
+	private Feature feature;
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+	}
+	
+	@Override
+	public void characters(char[] ch, int start, int length) throws SAXException {
+		// TODO Auto-generated method stub
+		super.characters(ch, start, length);
+	}
+	
+	@Override
+	public void endDocument() throws SAXException {
+		//return list;
+	}
+}
