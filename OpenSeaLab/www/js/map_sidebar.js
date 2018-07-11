6d2dcd8a72e40611dfdafc07e08dfaa7b2a6a622
kaskales <kaskalesuisse@gmail.com>
2018-07-11 11:26:23 +0200

    can capture location on a click in javascript

--- OpenSeaLab/www/js/map_sidebar.js
+++ OpenSeaLab/www/js/map_sidebar.js
@@ -1,3 +1,17 @@
 var map = L.map('map', {zoomControl:true}).setView([39.7392, -94.9847], 4);
 
 L.tileLayer.provider('Esri.OceanBasemap').addTo(map);
+
+function drawRectangle() {
+    document.getElementById("drawRecButton").style.color = "red";
+
+
+    map.on('click', function(e) {
+	    
+	    document.getElementById("drawRecButton").style.color = "blue";
+	    document.getElementById("recBound1").innerHTML = "Lat, Lon : "  + e.latlng.lat + ", " + e.latlng.lng;
+	});
+
+    rectangle =  new L.rectangle([  [17.853290, 34.980469], [10.876465, 14.853516]]);
+	map.addLayer(rectangle);
+}
