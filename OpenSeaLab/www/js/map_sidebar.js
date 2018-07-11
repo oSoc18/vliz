59d577c1c8f279d5930a89390c487d96e349b1a7
kaskales <kaskalesuisse@gmail.com>
2018-07-11 11:56:59 +0200

    can select and delete a rectangle

--- OpenSeaLab/www/js/map_sidebar.js
+++ OpenSeaLab/www/js/map_sidebar.js
@@ -2,16 +2,45 @@ var map = L.map('map', {zoomControl:true}).setView([39.7392, -94.9847], 4);
 
 L.tileLayer.provider('Esri.OceanBasemap').addTo(map);
 
-function drawRectangle() {
-    document.getElementById("drawRecButton").style.color = "red";
+var boundry1 = null;
+var boundry2 = null;
+var drawnRec = false;
 
+function selectCordRectangle() {
 
     map.on('click', function(e) {
-	    
-	    document.getElementById("drawRecButton").style.color = "blue";
-	    document.getElementById("recBound1").innerHTML = "Lat, Lon : "  + e.latlng.lat + ", " + e.latlng.lng;
+	    if(boundry1 == null){
+	    	document.getElementById("recBound1").innerHTML = "Boundry 1 Lat, Lon : "  + e.latlng.lat + ", " + e.latlng.lng;
+	    	boundry1 = e.latlng;
+	    } else if(boundry2 == null){
+	    	document.getElementById("recBound2").innerHTML = "Boundry 2 Lat, Lon : "  + e.latlng.lat + ", " + e.latlng.lng;
+	    	boundry2 = e.latlng;
+	    }	    
 	});
+    
+}
 
-    rectangle =  new L.rectangle([  [17.853290, 34.980469], [10.876465, 14.853516]]);
-	map.addLayer(rectangle);
+function clearCordRectangle(){
+	var boundry1 = null;
+	var boundry2 = null;
+	var drawnRec = false;
+	document.getElementById("recBound1").innerHTML = "";
+	document.getElementById("recBound2").innerHTML = "";
+}
+
+function drawRectangle(){
+	if(boundry1 != null && boundry2 != null && !drawnRec){
+    	rectangle =  new L.rectangle([  [boundry1.lat, boundry1.lng], [boundry2.lat, boundry2.lng]]);
+		map.addLayer(rectangle);
+		drawnRec = true;
+		
+    }else{
+    	alert("Select all the bounds!");
+    }
+    
+}
+
+function deleteRectangle(){
+	map.removeLayer(rectangle);
+    
 }
