55f8484c2aa2f7f063bc17ed32b86522f89f7552
Pietervdvn <pietervdvn@gmail.com>
2018-07-16 16:11:48 +0200

    Added tests

--- OpenSeaLab/tests/seabedhabitat/feature/ClippingTest.java
+++ OpenSeaLab/tests/seabedhabitat/feature/ClippingTest.java
@@ -16,11 +16,12 @@ public class ClippingTest {
 		
 		
 		Polygon poly = new Rectangle(0.1, 0.1, 0.9, 0.9).asPolygon();
-	//	Polygon poly0 = poly.clippedWith(r);
-	//	assertTrue(poly.toGeoJSON().equals(poly0.toGeoJSON()));
+		Polygon poly0 = poly.clippedWith(r);
+		assertTrue(poly.toGeoJSON().equals(poly0.toGeoJSON()));
 		
 		Polygon poly1 = new Rectangle(0.1, 0.1, 0.9, 2.1).asPolygon().clippedWith(r);
-		System.out.println(poly1);
+		String expected = "\"geometry\": { \"type\": \"Polygon\", \"coordinates\": [[ [0.1, 0.1], [2.0, 0.1], [2.0, 0.9], [0.1, 0.9] ]] }";
+		assertTrue(expected.equals(poly1.toGeoJSON()));
 		
 	}
 
