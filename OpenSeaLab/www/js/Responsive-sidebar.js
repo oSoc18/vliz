fe9122d0fd737037cae615acdfc75176b8fc60d1
TechnoWeb2018 <38584059+TechnoWeb2018@users.noreply.github.com>
2018-07-17 15:32:55 +0200

    Add Filters sidebar

--- OpenSeaLab/www/js/Responsive-sidebar.js
+++ OpenSeaLab/www/js/Responsive-sidebar.js
@@ -1,9 +1,11 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("responsive-main").style.marginRight = "250px";
+    document.getElementById("mySidenav").style.width = "500px";
+    document.getElementById("responsive-main").style.marginRight = "500px";
+
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("responsive-main").style.marginRight= "0";
+
 }
