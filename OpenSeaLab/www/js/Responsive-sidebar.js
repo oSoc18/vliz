dae020044090f770176f7ceafe8f589109d8cb02
TechnoWeb2018 <38584059+TechnoWeb2018@users.noreply.github.com>
2018-07-17 13:50:35 +0200

    Add responsive Sidebar in map.html

---
+++ OpenSeaLab/www/js/Responsive-sidebar.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("responsive-main").style.marginRight = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("responsive-main").style.marginRight= "0";
+}
