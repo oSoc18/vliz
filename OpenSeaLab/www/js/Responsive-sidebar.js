1b067bf00c122132cc32756709bf65ae23dd4636
TechnoWeb2018 <38584059+TechnoWeb2018@users.noreply.github.com>
2018-07-19 00:24:24 +0200

    Responsive Sibebar with bootstrap

--- OpenSeaLab/www/js/Responsive-sidebar.js
+++ OpenSeaLab/www/js/Responsive-sidebar.js
@@ -1,13 +1,20 @@
+$('.btn-expand-collapse').click(function(e) {
+				$('.navbar-primary').toggleClass('collapsed');
+});
+/*
 function openNav() {
-    document.getElementById("mySidenav").style.width = "700px";
-    document.getElementById("responsive-main").style.marginRight = "700px";
+    //document.getElementById("mySidenav").style.width = "700px";
+    //document.getElementById("responsive-main").style.marginRight = "700px";
+	document.getElementById("responsive-main").className = "col-lg-6";
+	document.getElementById("mySidenav").className = "col-lg-6";
 
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("responsive-main").style.marginRight= "0";
-
+    //document.getElementById("mySidenav").style.width = "0";
+    //document.getElementById("responsive-main").style.marginRight= "0";
+	document.getElementById("responsive-main").className = "col-lg-12";
+	document.getElementById("mySidenav").className = "col-lg-6 .d-none";
 }
 function openNavSum() {
     document.getElementById("myNav").style.height = "30%";
@@ -18,3 +25,4 @@ function closeNavSum() {
     document.getElementById("myNav").style.height = "0%";
 	document.getElementById("myNav").style.width = "0";
 }
+*/
