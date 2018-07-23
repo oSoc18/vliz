8a1e4a8658564ad10223a813d9f8ccc77cbfa0fd
TechnoWeb2018 <38584059+TechnoWeb2018@users.noreply.github.com>
2018-07-23 11:23:47 +0200

    Add download button and js code

---
+++ OpenSeaLab/www/js/Download.js
@@ -0,0 +1,17 @@
+function download(filename, text){
+	var element = document.createElement('a');
+	element.setAttribute('href','data:text/plain;charset=utf8,' + encodeURIComponent(text));
+	element.setAttribute('download',filename);
+	element.style.display ='none';
+	document.body.appendChild(element);
+	element.click();
+	document.body.removeChild(element);
+}
+// Start file download.
+document.getElementById("dwn-btn").addEventListener("click", function(){
+    // Generate download of statistics.txt file with some content
+    var text = document.getElementById("statsOutput").value;
+    var filename = "statistics.txt";
+    
+    download(filename, text);
+}, false);
